21701de27586655ec014190c9f0279a93d707afc
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-05 17:52:52 +0330

    🎉 nav component added

--- childrenedu/src/components/nav.jsx
+++ childrenedu/src/components/nav.jsx
@@ -4,6 +4,28 @@ export function NavBar() {
 
 
     return <div>
-        <Link/>
+        <ul>
+            <li>
+                <Link anylink="Contact"/>
+            </li>
+            <li>
+                <Link anylink="Student Life"/>
+            </li>
+            <li>
+                <Link anylink="Admissions"/>
+            </li>
+            <li>
+                <Link anylink="Academics"/>
+            </li>
+            <li>
+                <Link anylink="About Us"/>
+            </li>
+            <li>
+                <Link anylink="Home"/>
+            </li>
+        </ul>
+        <section>
+            <img src="../assets/Logo (1).png" alt="Logo of little learners" />
+        </section>
     </div>
 }
