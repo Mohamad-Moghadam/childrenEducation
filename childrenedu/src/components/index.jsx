8121fc45a849b46678c76713b7a940526ae642d6
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-06 00:23:21 +0330

    🔧 benefit added to index

--- childrenedu/src/components/index.jsx
+++ childrenedu/src/components/index.jsx
@@ -1,10 +1,12 @@
 import { NavBar } from "./nav";
 import { Main } from "./main";
+import { Benefits } from "./benefits";
 
 export function Index() {
 
     return <>
         <NavBar/>
         <Main/>
+        <Benefits/>
     </>
 }
