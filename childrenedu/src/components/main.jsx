9bb420a0f478b8973c3bd46e639b981daf64db88
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-05 23:46:27 +0330

    🎉 main part added

---
+++ childrenedu/src/components/main.jsx
@@ -0,0 +1,14 @@
+import { MainTxt } from "./mainTxt";
+
+export function Main() {
+
+
+    return <div>
+        <section>
+            <img src="../assets/Image Container (1).png" alt="Image of two children in our academy" />
+        </section>
+        <section>
+            <MainTxt/>
+        </section>
+    </div>
+}
---
+++ childrenedu/src/components/mainTxt.jsx
@@ -0,0 +1,39 @@
+export function MainTxt() {
+
+
+    return <div>
+        <section>
+            <p>
+                Welcome to Little Learners Academy
+            </p>
+            <h1>
+                Where Young Minds Blossom and <span>Dreams Take Flight. </span>
+            </h1>
+            <p>
+                Our kinder garden school provides a nurturing and stimulating environment, fostering a love for learning that lasts a lifetime. Join us as we embark on an exciting educational journey together!
+            </p>
+        </section>
+        <div>
+            <ul>
+                <li>
+                    +7000
+                    <h5>
+                        Students Passed Out
+                    </h5>
+                </li>
+                <li>
+                    +37
+                    <h5>
+                        Awards & Recognitions
+                    </h5>
+                </li>
+                <li>
+                    +15
+                    <h5>
+                        Experience Educators
+                    </h5>
+                </li>
+            </ul>
+        </div>
+    </div>
+}
